937bf2df7e8ef525a3c913dfbc841d774114b284
ttpfx <ttpfx123@gmail.com>
2023-05-19 11:28:38 +0800

    Initial commit

---
+++ src/main/java/com/ttpfx/Main.java
@@ -0,0 +1,11 @@
+package com.ttpfx;
+
+/**
+ * @author ttpfx
+ * @date 2023/5/19
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
